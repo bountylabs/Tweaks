707c8f8d593441518eeb6964a83675301659e856
Christian Niles <cniles@twitter.com>
2018-03-23 21:28:42 -0700

    Initial carthage support.

---
+++ FBTweaks-tvOS/FBTweaks_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  FBTweaks_tvOS.h
+//  FBTweaks-tvOS
+//
+//  Created by Christian Niles on 3/23/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FBTweaks_tvOS.
+FOUNDATION_EXPORT double FBTweaks_tvOSVersionNumber;
+
+//! Project version string for FBTweaks_tvOS.
+FOUNDATION_EXPORT const unsigned char FBTweaks_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBTweaks_tvOS/PublicHeader.h>
+
+
---
+++ FBTweaks/FBTweaks.h
@@ -0,0 +1,21 @@
+//
+//  FBTweaks.h
+//  FBTweaks
+//
+//  Created by Christian Niles on 3/23/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FBTweaks.
+FOUNDATION_EXPORT double FBTweaksVersionNumber;
+
+//! Project version string for FBTweaks.
+FOUNDATION_EXPORT const unsigned char FBTweaksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBTweaks/PublicHeader.h>
+
+
+#import <FBTweaks/FBTweak.h>
+#import <FBTweaks/FBTweakEnabled.h>
