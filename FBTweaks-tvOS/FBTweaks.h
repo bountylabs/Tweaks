695810847e33827c56e6474117fb93ef047bb8d4
Christian Niles <cniles@twitter.com>
2018-05-17 09:28:16 -0700

    Add dynamic framework targets for iOS and tvOS
    * Added shared schemes to support Carthage
    * Frameworks are compiled with FB_TWEAK_ENABLED=1. The framework is expected to be weakly linked, so this won’t affect client code.

---
+++ FBTweaks-tvOS/FBTweaks.h
@@ -0,0 +1,25 @@
+//
+//  FBTweaks.h
+//  FBTweaks
+//
+//  Created by Christian Niles on 3/23/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FBTweaks.
+FOUNDATION_EXPORT double FBTweaksVersionNumber;
+
+//! Project version string for FBTweaks.
+FOUNDATION_EXPORT const unsigned char FBTweaksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBTweaks/PublicHeader.h>
+
+
+#import <FBTweaks/FBTweakEnabled.h>
+#import <FBTweaks/FBTweak.h>
+#import <FBTweaks/FBTweakInline.h>
+#import <FBTweaks/FBTweakCollection.h>
+#import <FBTweaks/FBTweakCategory.h>
+#import <FBTweaks/FBTweakStore.h>
